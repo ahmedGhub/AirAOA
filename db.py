bea2e325a61d9b9e8ab9e4c1e78665bad331f89f
OmarRad <oradw074@uottawa.ca>
2020-04-04 22:44:09 -0400

    added docstrings with query descriptions

--- db.py
+++ db.py
@@ -23,7 +23,12 @@ cur = con.cursor()
 # con.commit()
 
 
-def q1():
+def q1_guestListQuery():
+    '''Give the details of all the guests who rented properties.
+    Please display the columns as guest name, rental type, rental
+    price, signing date, branch, payment type and payment status.
+    Sort by the payment type in ascending order and signing date in descending order.
+    '''
     query = """ SELECT first_name, last_name, property_type, price, signing_date, property.country, payment_type, status FROM(guest inner join rental_agreement
                 ON guest.username= rental_agreement.guest_username) 
                 JOIN property USING (property_id) 
@@ -34,16 +39,18 @@ def q1():
     cur.execute(query)
     return cur.fetchall()
 
-
-# TODO
-def q2():
+def q2_guestListView():
+    '''Create a view named GuestListView that gives the details of
+     all the guests. Please, sort the guests by the branch id and then by guest id.
+    '''
     query = """SELECT * FROM GuestListView"""
 
     cur.execute(query)
     return cur.fetchall()
 
 
-def q3():
+def q3_cheapestRental():
+    '''Display the details of the cheapest (completed) rental.'''
     query = """WITH min_price(value) AS (SELECT MIN(price) FROM price) 
             SELECT guest.first_name AS Guest_first_name, guest.last_name  AS Guest_last_name,
 			host.first_name AS Host_first_name, host.last_name AS Host_last_name,
@@ -57,7 +64,8 @@ def q3():
     return cur.fetchall()
 
 
-def q4():
+def q4_allProperties():
+    '''List all the properties rented and sort based on the branch id and review rating.'''
     query = """
             SELECT averageratings.avg_rating, rented_props.*
             FROM (SELECT DISTINCT property.*
@@ -73,7 +81,8 @@ def q4():
     return cur.fetchall()
 
 
-def q5():
+def q5_propertiesNotRented():
+    '''Find the properties that are already listed but not yet rented. Please, avoid duplications. '''
     query = """
             SELECT DISTINCT property.* 
             FROM property
@@ -85,7 +94,9 @@ def q5():
     return cur.fetchall()
 
 
-def q6():
+def q6_rented10th():
+    '''List all the details of all properties rented on the 10 th day of any month.
+    Ensure to insert dates in your table that correspond in order to run your query. '''
     query = """
             SELECT property.*  FROM property NATURAL JOIN  rental_agreement
 	        WHERE 10 BETWEEN EXTRACT (DAY FROM start_date) AND EXTRACT (DAY FROM end_date)
@@ -95,7 +106,10 @@ def q6():
     return cur.fetchall()
 
 
-def q7():
+def q7_employee15k():
+    '''List all the managers and the employees with salary greater than or
+    equal to $15000 by their ids, names, branch ids, branch names and salary.
+    Sort by manager id and then by employee id. '''
     query = """
            SELECT username, first_name, middle_name, last_name, branch_id, salary
             FROM employee
@@ -107,7 +121,8 @@ def q7():
     return cur.fetchall()
 
 
-def q8(username):
+def q8_guestBill(username):
+    '''Consider creating a simple bill for a guest stating the property type, host, address, amount paid and payment type. '''
     query = """
            SELECT guest.first_name AS Guest_first_name, guest.last_name  AS Guest_last_name,
 			host.first_name AS Host_first_name, host.last_name AS Host_last_name,
@@ -123,7 +138,8 @@ def q8(username):
     return cur.fetchall()
 
 
-def q9(phone_number, username):
+def q9_guestPhoneUpdate(phone_number, username):
+    '''Update the phone number of a guest. '''
     query = """
            UPDATE user_phone_guest SET phone_number = {0} WHERE username = '{1}'
             """
@@ -134,7 +150,10 @@ def q9(phone_number, username):
 # note here i am only calling the function without creating it
 
 
-def q10():
+def q10_firstNameFirst():
+    '''Create and test a user-defined function named FirstNameFirst that combines
+    two attributes of the guest named firstName and lastName into a concatenated
+    value named fullName [e.g., James and Brown will be combined to read James Brown].'''
     query = """
           SELECT username, FirstNameFirst(guest.username) AS Name FROM guest
             """
